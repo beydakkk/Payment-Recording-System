ec81f3b843d92f8c2cfc425dd83e2d79020a41c6
Beyda Güler <beydakk@gmail.com>
2023-07-19 20:05:06 +0300

    .

--- Main.py
+++ Main.py
@@ -7,6 +7,16 @@ class Main(QDialog):
         super().__init__()
         self.pencere = Ui_Dialog()
         self.pencere.setupUi(self)
+        self.pencere.pushButton.clicked.connect(self.handle_button_click)
+        self.pencere.pushButton_2.clicked.connect(self.handle_button_click_2)
+
+    def handle_button_click(self):
+        # This method will be called when the button is clicked
+        print("Button Clicked!")
+
+    def handle_button_click_2(self):
+        # This method will be called when the button is clicked
+        print("Button 2 Clicked!")
 
 app = QApplication([])
 pencere = Main()
